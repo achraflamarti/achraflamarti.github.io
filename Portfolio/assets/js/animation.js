dc698e9905c3989029861d20a8706a2bf1227d8f
Achraf LAMARTI <70669339+achraflamarti@users.noreply.github.com>
2022-11-11 00:35:10 +0100

    Update animation.js

--- Portfolio/assets/js/animation.js
+++ Portfolio/assets/js/animation.js
@@ -1,3 +1,10 @@
 function myFunction() {
-    window.open('../assets/cv/CV_AchrafLamarti.pdf');
+    window.open('../assets/cv/MyCV.pdf');
+}
+
+var element_navbar = document.querySelector('.nav-link');
+var navbar_collapse = document.querySelector('.navbar-collapse');
+
+function salam(){
+    navbar_collapse.classList.remove("show");
 }
